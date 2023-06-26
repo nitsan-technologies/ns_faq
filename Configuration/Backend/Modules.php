2936fb298cec8c178f9096d5d1aa9d9bdf5a5935
Vivek Solanki <vivek.nitsan@gmail.com>
2023-06-26 18:53:49 +0530

    [TASK] backend module with basetheme

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,21 +1,12 @@
 <?php
 
-use FaqModuleController;
-
-return [
-    'nitsan_module' => [
-        'labels' => 'LLL:EXT:ns_faq/Resources/Private/Language/BackendModule.xlf',
-        'icon' => 'EXT:ns_faq/Resources/Public/Icons/module-nsfaq.svg',
-        'iconIdentifier' => 'module-nsfaq',
-        'navigationComponent' => '@typo3/backend/page-tree/page-tree-element',
-        'position' => ['after' => 'web'],
-    ],
+$module = [
     'nitsan_nsfaqnitsan_configuration' => [
         'parent' => 'nitsan_module',
         'position' => ['before' => 'top'],
         'access' => 'admin,user,group',
         'path' => '/module/nitsan/NsFaqConfiguration',
-        'iconIdentifier'   => 'ns_faq-plugin-faq',
+        'iconIdentifier'   => 'nsfaq_icon',
         'labels' => 'LLL:EXT:ns_faq/Resources/Private/Language/locallang_faqconstants.xlf',
         'navigationComponent' => '@typo3/backend/page-tree/page-tree-element',
         'extensionName' => 'NsFaq',
@@ -34,17 +25,26 @@ return [
         'position' => ['before' => 'top'],
         'access' => 'user,group',
         'path' => '/module/nitsan/NsFaqNitsan',
-        'iconIdentifier' => 'ns_faq-plugin-faq',
+        'iconIdentifier' => 'nsfaq_icon',
         'labels' => 'LLL:EXT:ns_faq/Resources/Private/Language/locallang_faq.xlf',
         'navigationComponent' => '@typo3/backend/page-tree/page-tree-element',
         'extensionName' => 'NsFaq',
         'controllerActions' => [
             \NITSAN\NsFaq\Controller\FaqModuleController::class => [
                 'dashboard',
-                'faqList',
-                'faqCategories',
-                'faqBasicSettings', 
+                'faqList'
             ],
         ],
     ],
 ];
+
+if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ns_basetheme')) {
+    $module['nitsan_module'] = [
+        'labels' => 'LLL:EXT:ns_faq/Resources/Private/Language/BackendModule.xlf',
+        'iconIdentifier' => 'module-nsfaq',
+        'navigationComponent' => '@typo3/backend/page-tree/page-tree-element',
+        'position' => ['after' => 'web'],
+    ];
+}
+
+return $module;
--- ext_localconf.php
+++ ext_localconf.php
@@ -13,14 +13,16 @@ defined('TYPO3') || die('Access denied.');
 
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
-$iconRegistry->registerIcon(
-    'ns_faq-plugin-faq',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:ns_faq/Resources/Public/Icons/ns_faq.svg']
-);
-$iconRegistry->registerIcon(
-    'module-nsfaq',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:ns_faq/Resources/Public/Icons/module-nitsan.svg']
-);  
+$icons = [
+    'ns_faq-plugin-faq' => 'ns_faq.svg',
+    'module-nsfaq' => 'module-nitsan.svg',
+    'nsfaq_icon' => 'ns_faq.svg'
+];
 
+foreach ($icons as $identifier => $path) {
+    $iconRegistry->registerIcon(
+        $identifier,
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:ns_faq/Resources/Public/Icons/' . $path]
+    );
+}
