d4950f748ca3afb4ce6618b57df963b3b59111ff
NITSAN Technologies <sanjay@nitsan.in>
2023-06-27 16:09:22 +0530

    [TASK] Ready to release minor v12.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,6 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
+
+$EM_CONF['ns_faq'] = [
     'title' => '[NITSAN] Ultimate FAQ',
     'description' => 'One of the only feature-rich TYPO3 FAQ extension-ever. Live-Demo: https://demo.t3planet.com/t3t-extensions/faq/ You can download PRO version for more-features & free-support at https://t3planet.com/ns-faq-typo3-extension',
     'category' => 'plugin',
@@ -13,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '12.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '^12.0',
+            'typo3' => '12.0.0-12.4.99',
         ],
         'conflicts' => [
         ],
