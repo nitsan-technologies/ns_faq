e9a8c6b21f88f5d83cea975e2659b03d7210d745
Khushi Rupera <khushirupera.nitsan@gmail.com>
2024-04-23 14:58:46 +0530

    [TASK]Code cleanup and Improvement

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,6 +8,7 @@ CREATE TABLE tx_nsfaq_domain_model_faq (
 
 	faq_title varchar(255) DEFAULT '' NOT NULL,
 	faq_content text,
+	faqcategories int(11) unsigned DEFAULT '0' NOT NULL,
   accesstoken varchar(255) DEFAULT '' NOT NULL,
   sorting int(11) unsigned DEFAULT '0' NOT NULL,
   tstamp int(11) unsigned DEFAULT '0' NOT NULL,
