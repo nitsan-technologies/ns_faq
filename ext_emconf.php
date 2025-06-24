af3d53e32a58edb6fc910111d047daf16cd99bf5
NITSAN <info@nitsan.in>
2025-06-24 16:57:27 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_faq'] = [
-    'title' => 'Ultimate FAQ',
-    'description' => 'The Ultimate FAQ TYPO3 Extension helps in managing Frequently Asked Questions easy and efficient for your TYPO3 website. With its user-friendly backend module, you can effortlessly add, customize, and style the FAQs. It\'s fully compatible with the latest TYPO3 Version
+    'title' => 'FAQ Manager for TYPO3',
+    'description' => 'Easily manage and display Frequently Asked Questions on your TYPO3 website. This extension offers a user-friendly backend interface to create, customize, and organize FAQs efficiently. Compatible with the latest TYPO3 versions.',
 
-    *** Live Demo: https://demo.t3planet.com/t3t-extensions/faq/ *** Premium Version, Documentation & Free Support: https://t3planet.com/ns-faq-typo3-extension',
     'category' => 'plugin',
-    'author' => 'T3: Vivek Solanki, T3: Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
